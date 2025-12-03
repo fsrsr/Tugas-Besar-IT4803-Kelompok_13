87338526f946c5e25bf2d84acd3500b77596c664
RendraIlham <rend124ilham@gmail.com>
2025-12-03 08:38:06 +0700

    Saran asprak (yang di command)

--- Relation.h
+++ Relation.h
@@ -8,14 +8,38 @@ struct elmR{
     adrKey infoRC;
 };
 
-createListR_1030323430027();
-createElmR_();
+createElmR(adrKey ...)
+{
+    infoRC = kryword
+    nrxtR = nullptr
+}
 
-insertRFirst_103032430027();
-insertRAfter_103032430027();
-insertRLast_103032430027();
+
+//insertRelation(adrkey  k, adrpaper p){
+    //r = createElmR(k) // buat elm relationnya
+    //menghubungkan paper ke keyword
+     //baru insert
+
+    //if p->firstR == null{
+            //insertfirst
+    //else
+        //insertlast
+        //depan insertfirts, belakang last, dst. (all di paper child dan relation)
+//}
+//insertRFirst_103032430027(adrPaper p, adrR relation){
+    //P->firstR = relation
+//}
+//insertRAfter_103032430027(); //parameternya ada adrKey dan adrPaper (all insert)
+//insertRLast_103032430027(adrPaper p, adrR relation){
+    //adrR x = p->firstR;
+    //while x->nextR != nullptr{
+        //x = x->next
+    //}
+    //x->next = relation
+//}
 
 deleteRFirst_();
+
 deleteRAfter_();
 deleteRLast_();
 
@@ -24,3 +48,10 @@ viewR_();
 
 
 #endif // RELATION_H_INCLUDED
+
+
+// menu
+// user masukin judul paper dan isinya
+// nanya ke user, keyword satu satu
+
+// kalo keyword udh ada yaudah, kalo blm create trus dimasukin ke list keyword
