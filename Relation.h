baf95db7eb847bd5e42e5c934da5b83b409dc3e9
RendraIlham <rend124ilham@gmail.com>
2025-12-06 16:17:03 +0700

    Progress 3 new

--- Relation.h
+++ Relation.h
@@ -1,19 +1,26 @@
 #ifndef RELATION_H_INCLUDED
 #define RELATION_H_INCLUDED
+#include "Paper.h"
 #include "Keyword.h"
 
+typedef struct elmR *adrR;
 
+struct elmR{
+    adrR nextR;
+    adrPaper infoRC;
+};
 
-
-//
-
-
-
-
-
-
+void insertRelation(adrKey k, adrPaper p);
+void insertRFirst(adrPaper p, adrR relation);
+void insertRAfter();
+void insertRLast(adrPaper p, adrR relation);
+void deleteRFirst();
+void deleteRAfter();
+void deleteRLast();
+void findElmR();
+void viewR();
 
 #endif // RELATION_H_INCLUDED
 
 
-/
+
